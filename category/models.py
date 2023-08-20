c3649c441d15794da10fa4a906b6e6ca8e47c216
Edouard Kwizera <94765310+KwizeraEdouard@users.noreply.github.com>
2023-08-20 18:02:23 -0400

    added product model

--- category/models.py
+++ category/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 
-# // category model
-
+# Category model
 class Category(models.Model):
-    category_name = models.CharField(max_length= 50, unique=True)
-    slug = models.SlugField(max_length=100, unique = True)
-    description = models.TextField(max_length= 255)
-    cat_image = models.ImageField(upload_to = "photos/categories", blank=True)
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=255)
+    cat_image = models.ImageField(upload_to="photos/categories", blank=True)
 
-    class Meta: 
+    class Meta:
         verbose_name = "category"
         verbose_name_plural = "categories"
 
-def __str__(self):
-    return self.category_name()
-    
+    def __str__(self):
+        return self.category_name  
