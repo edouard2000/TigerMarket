b6b0216f816ed55edbc827f3a7cac3d90a41aeb0
Edouard Kwizera <94765310+KwizeraEdouard@users.noreply.github.com>
2023-08-23 22:43:40 -0400

    fetching products by categories

--- store/views.py
+++ store/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
-def store(request):
-    products = Product.objects.filter(is_available=True)
-    products_counts = products.count()
+def store(request, category_slug=None):
+    category = None
+    products = None
+
+    if category_slug is not None:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(categorry=category, is_available=True)
+        products_count = products.count()
+    else:
+        products = Product.objects.filter(is_available=True)
+        products_count = products.count()
+
     context = {
         "products": products,
-        "products_counts": products_counts
+        "products_count": products_count,
     }
     return render(request, 'store/store.html', context)
