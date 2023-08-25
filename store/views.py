df9a27fc62e9adf55ab2c8fafca3ede592a70bc2
Edouard Kwizera <94765310+KwizeraEdouard@users.noreply.github.com>
2023-08-25 11:49:56 -0400

    updated codes

--- store/views.py
+++ store/views.py
@@ -7,7 +7,7 @@ def store(request, category_slug=None):
 
     if category_slug is not None:
         category = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(categorry=category, is_available=True)
+        products = Product.objects.filter(category=category, is_available=True)
         products_count = products.count()
     else:
         products = Product.objects.filter(is_available=True)
